41b8f4a95d1825e1b4b1e313a86b5047354faec2
Marijn Suijten <marijns95@gmail.com>
2021-01-23 12:07:53 +0100

    AbsVol logging

--- profiles/audio/avrcp.c
+++ profiles/audio/avrcp.c
@@ -1620,6 +1620,8 @@ static uint8_t avrcp_handle_register_notification(struct avrcp *session,
 	if (len != 5)
 		goto err;
 
+	DBG("Requesting registration for event %#x", pdu->params[0]);
+
 	/* Check if event is supported otherwise reject */
 	if (!(session->supported_events & (1 << pdu->params[0])))
 		goto err;
@@ -1653,6 +1655,7 @@ static uint8_t avrcp_handle_register_notification(struct avrcp *session,
 		break;
 	case AVRCP_EVENT_VOLUME_CHANGED:
 		volume = media_transport_get_device_volume(dev);
+		DBG("Register for volume with initial value %d", volume);
 		if (volume < 0)
 			goto err;
 
@@ -1752,10 +1755,12 @@ static uint8_t avrcp_handle_set_absolute_volume(struct avrcp *session,
 	uint16_t len = ntohs(pdu->params_len);
 	int8_t volume;
 
+
 	if (len != 1)
 		goto err;
 
 	volume = pdu->params[0] & 0x7F;
+	DBG("volume=%d", volume);
 
 	media_transport_update_device_volume(session->dev, volume);
 
@@ -3971,6 +3976,8 @@ static gboolean avrcp_get_capabilities_resp(struct avctp *conn, uint8_t code,
 
 		events |= (1 << event);
 
+		DBG("Remote reports supported event %#x", event);
+
 		switch (event) {
 		case AVRCP_EVENT_STATUS_CHANGED:
 		case AVRCP_EVENT_TRACK_CHANGED:
@@ -3985,6 +3992,7 @@ static gboolean avrcp_get_capabilities_resp(struct avctp *conn, uint8_t code,
 				break;
 			/* fall through */
 		case AVRCP_EVENT_VOLUME_CHANGED:
+			DBG("Registering event %#x", event);
 			avrcp_register_notification(session, event);
 			break;
 		}
@@ -4075,6 +4083,7 @@ static struct avrcp_data *data_init(struct avrcp *session, const char *uuid)
 	data = g_new0(struct avrcp_data, 1);
 
 	rec = btd_device_get_record(session->dev, uuid);
+	sdp_record_print(rec);
 	if (rec == NULL)
 		return data;
 
@@ -4084,6 +4093,19 @@ static struct avrcp_data *data_init(struct avrcp *session, const char *uuid)
 	}
 
 	sdp_get_int_attr(rec, SDP_ATTR_SUPPORTED_FEATURES, &data->features);
+	error("Supported features %#x", data->features);
+	if (data->features & AVRCP_FEATURE_CATEGORY_1)
+		error("Supports AVRCP_FEATURE_CATEGORY_1");
+	if (data->features & AVRCP_FEATURE_CATEGORY_2)
+		error("Supports AVRCP_FEATURE_CATEGORY_2");
+	if (data->features & AVRCP_FEATURE_CATEGORY_3)
+		error("Supports AVRCP_FEATURE_CATEGORY_3");
+	if (data->features & AVRCP_FEATURE_CATEGORY_4)
+		error("Supports AVRCP_FEATURE_CATEGORY_4");
+	if (data->features & AVRCP_FEATURE_PLAYER_SETTINGS)
+		error("Supports AVRCP_FEATURE_PLAYER_SETTINGS");
+	if (data->features & AVRCP_FEATURE_BROWSING)
+		error("Supports AVRCP_FEATURE_BROWSING");
 	sdp_list_free(list, free);
 
 	return data;
@@ -4152,6 +4174,7 @@ static void target_init(struct avrcp *session)
 				(1 << AVRCP_EVENT_TRACK_REACHED_END) |
 				(1 << AVRCP_EVENT_SETTINGS_CHANGED);
 
+	// Remote device supports receiving volume notifications
 	if (target->features & AVRCP_FEATURE_CATEGORY_2)
 		session->supported_events |=
 				(1 << AVRCP_EVENT_VOLUME_CHANGED);
